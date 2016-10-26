19faeacfdd77b5e5b6216886b9bd93a45c30420d
Joy McNally <bjaerickson@gmail.com>
2016-10-25 22:52:27 -0700

    finished first complete draft 10/25
    fully functioning, double check before submitting

--- stage2_project.py
+++ stage2_project.py
@@ -1,80 +1,88 @@
 # IPND Stage 2 Final Project
 
-# For this project, you'll be building a Fill-in-the-Blanks quiz.
-# Your quiz will prompt a user with a paragraph containing several blanks.
-# The user should then be asked to fill in each blank appropriately to complete the paragraph.
-# This can be used as a study tool to help you remember important vocabulary!
-
-# Note: Your game will have to accept user input so, like the Mad Libs generator,
-# you won't be able to run it using Sublime's `Build` feature.
-# Instead you'll need to run the program in Terminal or IDLE.
-# Refer to Work Session 5 if you need a refresher on how to do this.
-
-# To help you get started, we've provided a sample paragraph that you can use when testing your code.
-# Your game should consist of 3 or more levels, so you should add your own paragraphs as well!
-
+# for Level 1:
 sample = "A ___1___ is created with the def keyword. You specify the inputs a ___1___ takes by adding ___2___ separated by commas between the parentheses. ___1___s by default return ___3___ if you don't specify the value to return. ___2___ can be standard data types such as string, number, dictionary, tuple, and ___4___ or can be more complicated such as objects and lambda functions."
+correct_answers1 = ["function","parameters","None","list"]
 
-# The answer for ___1___ is 'function'. Can you figure out the others?
+# for Level 2:
+# I had a lot of trouble with the creative part of this project and felt just making more vocab tests was boring (haha)
 
-# We've also given you a file called fill-in-the-blanks.pyc which is a working version of the project.
-# A .pyc file is a Python file that has been translated into "byte code".
-# This means the code will run the same as the original .py file, but when you open it
-# it won't look like Python code! But you can run it just like a regular Python file
-# to see how your code should behave.
+level2 = "The Eiffel Tower is in ___1___, France. The difference between ___2___ (like humans and gorillas) and monkeys is that ___2___, don't have tails. The mitochondria is the powerhouse of the ___3___. Reggae, Bob Marley, and a killer bobsled team, originated in the country of ___4___. Steph Curry is a famous ___5___ player on the Golden State Warriors. The Rock refers to both the prison on the ___6___ of Alcatraz and top shelf Nicolas Cage film." 
+correct_answers2 = ["Paris","apes","cell","Jamaica","basketball","island"]
 
-# Hint: It might help to think about how this project relates to the Mad Libs generator you built with Sean.
-# In the Mad Libs generator, you take a paragraph and replace all instances of NOUN and VERB.
-# How can you adapt that design to work with numbered blanks?
+# for Level 3:
+level3 = "In the first Stage of the IPND, we made a webpage using ___1___ and CSS."
+correct_answers3 = ["HTML"]
 
-# If you need help, you can sign up for a 1 on 1 coaching appointment: https://calendly.com/ipnd-1-1/20min/
+# for all levels:
+correct_answers = [] # this is used in the get_answers function
+to_be_replaced = ["___1___","___2___","___3___","___4___","___5___","___6___"]
 
+# introduction
 user_name = raw_input("Hello! What is your name? ")
-print "Welcome " + user_name + ", I hope you enjoy my Stage 2 project for Udacity's IPND"
+print "\nWelcome " + user_name + ", I hope you enjoy my Stage 2 project for Udacity's IPND"
 level = raw_input("Would you like to play Level 1, 2, or 3? ")
 
-def level_selection(chosen_level):
-    if chosen_level == "1":
-        # start level 1
-        print "Great! You chose Level " + chosen_level + ". Let's get started!"
-    if chosen_level == "2":
-        # start level 2
-        print "Great! You chose Level " + chosen_level + ". Let's get started!"
-    if chosen_level == "3":
-        # start level 3
-        print "Great! You chose Level " + chosen_level + ". Let's get started!"
-    else if chosen_level != "1" or chosen_level != "2" or chosen_level != "3":
-        print "Whoops! You'll need to start over. Please type in a number from 1 - 3."
+def get_answers(level):
+# pairs answers with chosen level
+    correct = []
+    if level == 1:
+        correct = correct_answers1
+    if level == 2:
+        correct = correct_answers2
+    if level == 3:
+        correct = correct_answers3
+    return correct
 
-level_selection(level)
-
-to_be_replaced = ["___1___","___2___","___3___","___4___"]
-correct_answers = ["function","parameters","None","list"]
-
-def level1_quiz(starting_string):
-    print "In the following paragraph, fill in the blanks with appropriate keywords."
-    print starting_string
-    string_as_list = starting_string.split()
-    # first blank
-
-def ask_and_check(blank_number): # blank_number = 1, 2, 3, 4
-# this function asks user for answer and checks if it is correct, user is allowed
-# five attempts
+def ask_and_check(blank_number, level): # blank_number = 1, 2, 3, 4
+# asks user for answer and checks if it is correct, user is allowed five attempts
     attempt = 0
+    correct_answers = get_answers(level)
+    index = (blank_number - 1) # corrects for index vs number in list
     while attempt < 5:
-        answer = raw_input("What goes in place of " + to_be_replaced[blank_number] + "? ")
-        if answer == correct_answers[blank_number]:
-            print "Good job!"
-            print answer
+        answer = raw_input("\nWhat goes in place of " + to_be_replaced[index] + "? ")
+        if answer == correct_answers[index]:
+            print "\nGood job!\n"
+            return answer
             break
         else:
             attempt += 1
         if attempt < 4:
             print "Nope, try again!"
         if attempt == 4:
-            print "Uh oh, only one more guess!"
+            print "Uh oh, only one more guess! \nHint: The first letter of the correct answer is " + correct_answers[index][0]
         if attempt == 5:
             print "Game over!"
+            exit() # looked this up on stackoverflow via Google
 
+def quiz(starting_string, number_of_blanks, level):
+# replaces blanks, i.e. ___x___, with correct answers 
+    print "In the following paragraph, fill in the blanks with appropriate keywords.\n"
+    print starting_string
+    blank = 1 # i.e. ___1___
+    while blank <= number_of_blanks:
+        replacement = ask_and_check(blank, level)
+        blank_to_be_replaced = "___" + str(blank) + "___"
+        starting_string = starting_string.replace(blank_to_be_replaced, replacement)
+        print starting_string
+        blank += 1
+    if blank > number_of_blanks:
+        print "\nCongratulations! You win!"
 
-level1_quiz(sample)
+def level_selection(chosen_level):
+    if chosen_level == "1":
+        # start level 1
+        print "\nGreat! You chose Level " + chosen_level + ". \nThis Level uses the sample paragraph provided by the course. \nLet's get started! \n***Please note that answers are case sensitive!***"
+        quiz(sample, 4, 1)
+    if chosen_level == "2":
+        # start level 2
+        print "\nGreat! You chose Level " + chosen_level + ". \nLet's get started! \n***Please note that answers are case sensitive!***"
+        quiz(level2, 6, 2)
+    if chosen_level == "3":
+        # start level 3
+        print "\nGreat! You chose Level " + chosen_level + ". \nLet's get started! \n***Please note that answers are case sensitive!***"
+        quiz(level3, 1, 3)
+    elif chosen_level != "1" and chosen_level != "2" and chosen_level != "3":
+        print "\nWhoops! You'll need to start over. Please type in a number from 1 - 3."
+
+level_selection(level)
--- stage2_project_tests.py
+++ stage2_project_tests.py
@@ -1,21 +1,5 @@
-to_be_replaced = ["___1___","___2___","___3___","___4___"]
-correct_answers = ["function","parameters","None","list"]
+my_list = ["one","two","three"]
 
-def ask_for_input(blank_number): # blank_number = 1, 2, 3, 4
-    attempt = 0
-    while attempt < 5:
-        answer = raw_input("What goes in place of " + to_be_replaced[blank_number] + "? ")
-        if answer == correct_answers[blank_number]:
-            print "Good job!"
-            print answer
-            break
-        else:
-            attempt += 1
-        if attempt < 4:
-            print "Nope, try again!"
-        if attempt == 4:
-            print "Uh oh, only one more guess!"
-        if attempt == 5:
-            print "Game over!"
-
-ask_for_input(0)
+print my_list
+print my_list[1]
+print my_list[1][1]
