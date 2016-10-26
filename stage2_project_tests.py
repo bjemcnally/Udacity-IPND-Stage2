e974bbae52c7aca4e0eddd003b9e432bc3db83fe
Joy McNally <bjaerickson@gmail.com>
2016-10-26 14:01:09 -0700

    ready to submit
    fixed Level 3 string and added component that allows the user to choose the level of difficulty (i.e. the number of attempts they get for each question)

--- stage2_project_tests.py
+++ stage2_project_tests.py
@@ -1,5 +1,13 @@
-my_list = ["one","two","three"]
+def difficulty_value(difficulty):
+# translates user's chosen diffulty to numerical value (i.e. number of allowed attempts)
+    if difficulty == "Easy":
+        allowed_attempts = 5
+    if difficulty == "Medium":
+        allowed_attempts = 3
+    if difficulty == "Hard":
+        allowed_attempts = 2
+    if difficulty == "Easy" and difficulty == "Medium" and difficulty == "Hard":
+        print "\nWhoops! You'll need to start over. Please choose a difficulty from Easy, Medium, or Hard (case sensitive!)."
+    return allowed_attempts
 
-print my_list
-print my_list[1]
-print my_list[1][1]
+print difficulty_value("Easy")
