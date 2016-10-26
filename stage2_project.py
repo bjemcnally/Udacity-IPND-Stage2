94ed2fe2035d41d3612137a31369dc6fd1ea205f
Joy McNally <bjaerickson@gmail.com>
2016-10-26 14:06:06 -0700

    ready to submit

--- stage2_project.py
+++ stage2_project.py
@@ -21,7 +21,22 @@ to_be_replaced = ["___1___","___2___","___3___","___4___","___5___","___6___"]
 # introduction
 user_name = raw_input("Hello! What is your name? ")
 print "\nWelcome " + user_name + ", I hope you enjoy my Stage 2 project for Udacity's IPND"
-level = raw_input("Choose a level: \nLevel 1: IPND Stage 2 Vocab Quiz \nLevel 2: General Knowledge Quiz\nLevel 3: Farm Animals Quiz \nWould you like to play Level 1, 2, or 3? ")
+level = raw_input("\nChoose a level: \nLevel 1: IPND Stage 2 Vocab Quiz \nLevel 2: General Knowledge Quiz\nLevel 3: Farm Animals Quiz \nWould you like to play Level 1, 2, or 3? ")
+difficulty = raw_input("\nChoose a difficulty: \nEasy = 5 guesses \nMedium = 3 guesses \nHard = 1 guess \nWould you like to play Easy, Medium, or Hard? ")
+
+def difficulty_value(difficulty):
+# translates user's chosen diffulty to numerical value (i.e. number of allowed attempts)
+    attempts = 0
+    if difficulty == "Easy":
+        attempts = 5
+    if difficulty == "Medium":
+        attempts = 3
+    if difficulty == "Hard":
+        attempts = 1
+    if difficulty != "Easy" and difficulty != "Medium" and difficulty != "Hard":
+        print "\nWhoops! You'll need to start over. Please choose a difficulty from Easy, Medium, or Hard (case sensitive!)."
+        exit()
+    return attempts
 
 def get_answers(level):
 # pairs answers (output) with chosen level (input) in ask_and_check function
@@ -35,7 +50,7 @@ def get_answers(level):
         correct = correct_answers3
     return correct
 
-def ask_and_check(blank_number, level): # blank_number = 1, 2, 3, 4, etc
+def ask_and_check(blank_number, level, allowed_attempts): # blank_number = 1, 2, 3, 4, etc
 # asks user for answer and checks if it is correct, user is allowed five attempts
 # blank_number refers to ___1___, ___2___, etc in each level's fill in the blank string (above)
 # if answer is correct, outputs correct answer, otherwise prints prompts and/or hints 
@@ -43,7 +58,7 @@ def ask_and_check(blank_number, level): # blank_number = 1, 2, 3, 4, etc
     attempt = 0
     correct_answers = get_answers(level)
     index = (blank_number - 1) # corrects for index vs number in list
-    while attempt < 5:
+    while attempt < allowed_attempts:
         answer = raw_input("\nWhat goes in place of " + to_be_replaced[index] + "? ")
         if answer == correct_answers[index]:
             print "\nGood job! Let's keep going.\n"
@@ -51,6 +66,9 @@ def ask_and_check(blank_number, level): # blank_number = 1, 2, 3, 4, etc
             break
         else:
             attempt += 1
+        if attempt == allowed_attempts:
+            print "\nWrong. Game over!"
+            exit() # looked this up on stackoverflow via Google
         if attempt < 3:
             print "Nope, try again!"
         if attempt == 3:
@@ -58,11 +76,8 @@ def ask_and_check(blank_number, level): # blank_number = 1, 2, 3, 4, etc
         if attempt == 4:
             index_last_character = len(correct_answers[index]) - 1
             print "Oh no, only one more guess. \nHere's another hint: The last letter of the correct answer is " + correct_answers[index][index_last_character]
-        if attempt == 5:
-            print "\nWrong again. Game over!"
-            exit() # looked this up on stackoverflow via Google
-
-def quiz(starting_string, number_of_blanks, level):
+        
+def quiz(starting_string, number_of_blanks, level, allowed_attempts):
 # replaces blanks, i.e. ___x___, with correct answers 
 # starting_string is each level's string, other variables are (hopefully) self explanatory
 # output is start_string with correct replacements
@@ -70,7 +85,7 @@ def quiz(starting_string, number_of_blanks, level):
     print starting_string
     blank = 1 # i.e. ___1___
     while blank <= number_of_blanks:
-        replacement = ask_and_check(blank, level)
+        replacement = ask_and_check(blank, level, allowed_attempts)
         blank_to_be_replaced = "___" + str(blank) + "___"
         starting_string = starting_string.replace(blank_to_be_replaced, replacement)
         print starting_string
@@ -78,23 +93,24 @@ def quiz(starting_string, number_of_blanks, level):
     if blank > number_of_blanks:
         print "\nCongratulations! You win!"
 
-def level_selection(chosen_level):
+def level_selection(chosen_level, chosen_difficulty):
 # takes users chosen level from introduction
 # prints level's prompt
 # runs 'quiz' function with correct parameters
+    allowed_attempts = difficulty_value(chosen_difficulty)
     if chosen_level == "1":
         # start level 1
-        print "\nGreat! You chose Level " + chosen_level + ". \nThis Level uses the sample paragraph provided by the course. \nLet's get started! \n***Please note that answers are case sensitive!***"
-        quiz(sample, 4, 1)
+        print "\nGreat! You chose Level " + chosen_level + " on " + chosen_difficulty + " \nThis Level uses the sample paragraph provided by the course. \nLet's get started! \n***Please note that answers are case sensitive!***"
+        quiz(sample, 4, 1, allowed_attempts)
     if chosen_level == "2":
         # start level 2
-        print "\nGreat! You chose Level " + chosen_level + ". \nLet's get started! \n***Please note that answers are case sensitive!***"
-        quiz(level2, 6, 2)
+        print "\nGreat! You chose Level " + chosen_level + " on " + chosen_difficulty + ". \nLet's get started! \n***Please note that answers are case sensitive!***"
+        quiz(level2, 6, 2, allowed_attempts)
     if chosen_level == "3":
         # start level 3
-        print "\nGreat! You chose Level " + chosen_level + ". \nLet's get started! \n***Please note that answers are case sensitive!***"
-        quiz(level3, 6, 3)
+        print "\nGreat! You chose Level " + chosen_level + " on " + chosen_difficulty + ". \nLet's get started! \n***Please note that answers are case sensitive!***"
+        quiz(level3, 6, 3, allowed_attempts)
     if chosen_level != "1" and chosen_level != "2" and chosen_level != "3":
-        print "\nWhoops! You'll need to start over. Please type in a number from 1 - 3."
+        print "\nWhoops! Looks like you chose an invalid Level and will need to start over."
 
-level_selection(level)
+level_selection(level, difficulty)
